f118597956e6566d7523c2d0a3baf1644f1ca15b
dinasalsabilla <dinasalsabilla05@gmail.com>
2025-04-09 17:11:51 +0700

    menambahkan class TeamMembers103022300153

--- modul7_kelompok6/Program.cs
+++ modul7_kelompok6/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
+using static modul7_kelompok6.DataMahasiswa103022300153;
 
 namespace modul7_kelompok6
 {
@@ -42,11 +43,51 @@ namespace modul7_kelompok6
         }
     }
 
+    class TeamMembers103022300153
+    {
+        internal class Members
+        {
+            public List<Members> Team { get; set; }
+            public Members() { }
+            public Members(List<Members> members)
+            { 
+                this.members = members;
+            }
+
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public string nim { get; set; }
+        }
+
+        internal class Team
+        {
+            public List<Members> Team { get; set; }
+        }
+
+        internal static void ReadJSON()
+        {
+            string filePath = "tp7_2_103022300153.json";
+            string jsonContent = File.ReadAllText(filePath);
+            Team data = JsonSerializer.Deserialize<Team>(jsonContent);
+
+            Console.WriteLine("Daftar member team:");
+            for (int i = 0; i <= Team.Members.Count; i++)
+            {
+                Console.WriteLine("Member " + i + " : Nama depan: " + t.firstName + ", Nama belakang: " + t.lastName + ", Gender: " + t.gender + ", Age: " + t.age + ", NIM: " + t.nim);
+                i++;
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             DataMahasiswa103022300153.ReadJSON();
+            Console.WriteLine();
+            TeamMembers103022300153.ReadJSON();
         }
     }
 }
